56cff0d27ac0165f25b1035cd31b8cbf1c32de65
Glory Bassey <117596977+Annytech001@users.noreply.github.com>
2025-05-20 23:00:20 +0000

    checkdupliacteregistration

--- src/app.py
+++ src/app.py
@@ -62,7 +62,7 @@ def signup_for_activity(activity_name: str, email: str):
     # Get the specificy activity
     activity = activities[activity_name]
 
-# Prevent duplicate registration
+# Validate student is not already signed up
     if email in activity["participants"]:
         raise HTTPException(status_code=400, detail="Student already registered for this activity")
 
